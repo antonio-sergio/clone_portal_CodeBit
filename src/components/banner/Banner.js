e4bb0c790afe23fc77a6b77daaacd1042ae1b5d6
Antonio Alves <antonioalves.dev@outlook.com>
2021-11-09 14:51:35 -0300

    implementaçao do banner da home

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,7 +1,35 @@
 import React, {Component} from 'react';
+import "./Banner.css";
+import BannerImg from './img/happy-face-1.png'
 
 export default class Banner extends Component{
     render(){
-        return <h1>I'm a banner!</h1>
+        return (
+            <div className="banner">
+                <div>
+                    <h1>Tecnologia deve ter uma causa</h1>
+                    <h2>e as causas merecem as melhores tecnologias</h2>
+                </div>
+                <div className="who-are-we">
+                    <h3>
+                        <span>///</span>
+                        QUEM SOMOS
+                    </h3>
+                    <p>
+                        Se educação é a arma mais poderosa para mudar o mundo, então tecnologia é seu catalisador. 
+                        Acreditando nisso, nos tornamos fornecedores de soluções para <span> startups, fundações empresariais,
+                        universidades, organizações do terceiro setor e iniciativas de projetos com cunho social</span>. 
+                        Atuando nesse mercado conseguimos compartilhar experiências e usar a tecnologia para viabilizar 
+                        um conjunto de iniciativas que visam levar mais educação, saúde e conhecimento para todos os
+                        setores da sociedade.
+                    </p>
+                    <p>
+                        <span>Somos especialistas em criar tecnologia para pessoas</span>, para melhorar a vida delas, possibilitando 
+                        capacitações, disseminação do conhecimento, inscrição em programas sociais, prêmios culturais e 
+                        demais iniciativas que ajudam a transformar o mundo em que vivem.
+                    </p>
+                </div>
+            </div>
+        )
     }
 }
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,10 +1,28 @@
 import React, {Component} from 'react';
+import headImg from './img/head-menu-bar-codebit-large.png';
+import certifiedImg from './img/b-corporation.png';
+import awsImg from './img/aws-consulting-partner.png';
+import './Menu.css'
 
 export default class Menu extends Component{
     render(){
         return (
             <div className="nav-menu">
-                
+                <div className="logo">
+                    <img src={headImg} alt="logo da codebit" ></img>
+                </div>
+                <div className="nav-links">
+                    <ul>
+                        <li className="active"><a href="/">Início</a></li>
+                        <li><a href="/">Quem Somos</a></li>
+                        <li><a href="/">Contato</a></li>
+                        <li><a href="/">Blog</a></li>
+                    </ul>
+                </div>
+                <div className="certifieds">
+                    <img src={certifiedImg} alt="logo de certificado B corporation"></img>
+                    <img src={awsImg} alt="logo aws consulting partner"></img>
+                </div>
             </div>
         )
     }
